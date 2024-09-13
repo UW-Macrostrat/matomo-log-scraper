41c29875a9291ccb6f8ad167630c19308dd77067
Cannon Lock <clock@wisc.edu>
2024-09-13 14:09:39 -0500

    Initial Commit

--- import.sh
+++ import.sh
@@ -7,7 +7,7 @@ ROTATED_LOG_FILE="$LOG_PATH.1"
 if [ -f "$ROTATED_LOG_FILE" ]; then
     echo "Log rotation successful. Processing the rotated log file."
     # Call the import script
-    python3 /app/import_logs.py "$ROTATED_LOG_FILE" --url "$MATOMO_URL" --token-auth "$API_TOKEN"
+    python3 /app/import_logs.py --dry-run "$ROTATED_LOG_FILE" --url "$MATOMO_URL" --token-auth "$API_TOKEN"
 else
     echo "Rotated log file not found."
     exit 1
