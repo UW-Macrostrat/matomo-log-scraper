5b48e3475d644d8bf60eee7eaf568348bc2f7792
Cannon Lock <clock@wisc.edu>
2024-09-30 14:13:39 -0500

    Signal traefik process to rotate logs

--- import.sh
+++ import.sh
@@ -8,6 +8,19 @@ if [ -f "$ROTATED_LOG_FILE" ]; then
     echo "Log rotation successful. Processing the rotated log file."
     # Call the import script
     python3 /app/import_logs.py --dry-run "$ROTATED_LOG_FILE" --url "$MATOMO_URL" --token-auth "$API_TOKEN"
+
+    # Find the PID of the traefik process
+    TRAEFIK_PID=$(pgrep traefik)
+
+    # Check if the PID was found
+    if [ -n "$TRAEFIK_PID" ]; then
+        # Send USR1 signal to the traefik process
+        kill -USR1 "$TRAEFIK_PID"
+        echo "Sent USR1 signal to traefik process with PID $TRAEFIK_PID"
+    else
+        echo "Traefik process not found."
+        exit 1
+    fi
 else
     echo "Rotated log file not found."
     exit 1
